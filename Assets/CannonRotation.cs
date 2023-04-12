1f97995feff366f900b66bee5b16853a2c66b1e0
Sobyn <evenfosser95@gmail.com>
2023-04-13 00:24:32 +0200

    add project files
    arena, player controller, rotatable turret

---
+++ Assets/CannonRotation.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CannonRotation : MonoBehaviour
+{
+    public float rotationOffset = 270;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Get mouse position
+        Vector3 mousePos = Input.mousePosition;
+        mousePos.z = 0;
+        Vector3 objectPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        mousePos.x = mousePos.x - objectPos.x;
+        mousePos.y = mousePos.y - objectPos.y;
+
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle + rotationOffset));
+
+        Vector3 targetPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        targetPos.z = 0;
+    }
+}
---
+++ Assets/playerMovement.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+    public float movementSpeed = 5.0f;
+    public Rigidbody2D rb;
+    Vector2 movement;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Set X and Y axis
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+    }    
+    
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * movementSpeed * Time.fixedDeltaTime);    }
+}
