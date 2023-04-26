d0136892872800ad96de735b9198a64ecee73d01
Sobyn <evenfosser95@gmail.com>
2023-04-26 22:07:17 +0200

    add knockback to bullet firing

--- Assets/Scripts/FireWithDelay.cs
+++ Assets/Scripts/FireWithDelay.cs
@@ -4,19 +4,35 @@ using UnityEngine;
 
 public class FireWithDelay : MonoBehaviour
 {
-    [Tooltip("Prefab rigidbody to spawn")][SerializeField]
+    [Tooltip("Prefab rigidbody to spawn")]
+    [SerializeField]
     private Rigidbody2D bullet;
 
-    [Tooltip("Empty object location to fire from")][SerializeField]
+    [Tooltip("Empty object location to fire from")]
+    [SerializeField]
     private Transform barrel;
 
-    [Tooltip("Bullet speed (Default: 500)")][Range(0f, 1000f)]
+    [Tooltip("Bullet speed (Default: 500)")]
+    [Range(0f, 1000f)]
     [SerializeField] private float bulletSpeed = 500f;
 
-    [Tooltip("Cooldown in seconds")][Range(0f, 10f)]
+    [Tooltip("Cooldown in seconds")]
+    [Range(0f, 10f)]
     [SerializeField] private float cooldown = 1f;
 
     private float nextFire = 0f;
+
+    [Tooltip("Knockback force (Default: 300)")]
+    [Range(0f, 1000f)]
+    [SerializeField] private float knockBack = 300f;
+
+    [SerializeField]
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        //rb = gameObject.GetComponent<Rigidbody2D>();
+    }
 
 
     void Update()
@@ -32,6 +48,9 @@ public class FireWithDelay : MonoBehaviour
 
             //add force to the bullet in the direction of the barrel's right side, with set speed
             spawnedBullet.AddForce(barrel.right * bulletSpeed);
+            
+            //add force to the player in the opposite direction
+            rb.AddForce(-barrel.right * knockBack);
         }
     }
 }
